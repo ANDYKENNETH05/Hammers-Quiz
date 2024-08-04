7ea9ed35161ee56ff7d19b9eb449c6bc4bfd2e14
Andy kenneth <andy.kenneth@binghamuni.edu.ng>
2024-08-04 20:22:49 +0100

    flask

--- Hammers Quiz/app.py
+++ Hammers Quiz/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "quiz hammer"
+
 
 @app.route("/")
 def index():
-    return"<h1>new flask application<h1>"
+    return render_template("base.html")
 
 
 if __name__ =='__main__':
